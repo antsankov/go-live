5374a1efac0aee53bbcc2b6fb3395144200fb23b
Mclark1985 <78991385+Mclark1985@users.noreply.github.com>
2022-03-19 19:50:28 -0500

    Update server.go

--- lib/server.go
+++ lib/server.go
@@ -66,3 +66,71 @@ func useCache(h http.Handler) http.Handler {
 	}
 	return http.HandlerFunc(fn)
 }
+Track
+
+Get informations from Track Object (Artist, Title, Duration, ...)
+$track->getArtist();
+$track->getTitle();
+$track->getDuration();
+Get current Track
+$api->getPlaybackInfo()->getCurrentTrack();
+Get next Track
+$api->getPlaybackInfo()->getNextTrack();
+Get previous Track
+$api->getPlaybackInfo()->getPreviousTrack();
+Get last played Songs
+//all last played songs
+$api->getLastPlayed();
+//filters out songs that were not sent to the server
+$api->getLastPlayed(true);
+Get Artwork of current or next Track
+$api->getTrackArtwork();
+$api->getNextTrackArtwork();
+Set next Track
+$api->setNextTack(123);
+Read Track data by filename
+$api->readTag("C:\Music\song.mp3");
+Playlist
+
+Get data from Playlist Object (Tracks, Track count)
+$playlist->getTracks();
+$playlist->getCount();
+Get Playlist (provides all information, may be slow for some large Playlists)
+//all songs
+$api->getPlaylist();
+//only songs from 5-10
+$api->getPlaylist(5, 10);
+Get Playlist (doesn't provide all information)
+//all songs
+$api->getPlaylist2();
+//only songs the first 10 songs
+$api->getPlaylist2(10);
+Microphone
+
+Get Microphone status
+$api->getMicrophone();
+Enable/Disable Microphone
+$api->setMicrophone(true);
+$api->setMicrophone(false);
+Playback
+
+Get data from Playback Object (Position, Length, State, ...)
+$playback->getPosition();
+$playback->getLength();
+$playback->getState();
+Get Playback Object
+$api->getPlaybackInfo()->getPlayback();
+Encoder
+
+Get data from Encoder Object (Status, Error, ...)
+$encoder->getStatus();
+$encoder->getError();
+Get Encoder Object
+$api->getEncoderStatus();
+Player
+
+Get data from Player Object (Version, Uptime)
+$player->getVersion();
+$player->getUptime();
+Get Player Object
+$api->getStatus();
